10e375969fe042688642b60de56400e92a005500
abhi-bhavsar <163317055+abhi-bhavsar@users.noreply.github.com>
2024-03-17 13:00:55 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,17 @@
+// input output stream
+#include <iostream>
+
+using namespace std;
+int main()
+{
+
+    int num1, num2;
+    cout << "the val of num1 is: ";
+    cin >> num1; //  '>>' is extraction operator
+    cout << "the val of num2 is: ";
+    cin >> num2; // '<<'  is insertion operator
+    cout << "the sum is: " << num1 + num2 << endl;
+    cout << "size of long double is:" << sizeof(long double);
+    return 0;
+    // pointer to pointer
+}
