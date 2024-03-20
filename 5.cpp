45351128dd5bef5d4835c37cb5a3cd8c69c271ca
abhi-bhavsar <163317055+abhi-bhavsar@users.noreply.github.com>
2024-03-20 20:48:45 +0530

    reference variable and typecasting

---
+++ 5.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+// header fioes and operators
+using namespace std;
+// reference variable and typecasting
+int c = 50;
+int main()
+{
+  /* int a,b,c;
+   cout<<"the val of a is : "<<endl;
+   cin>>a;
+   cout<<"the val of b is : "<<endl;
+    cin>>b;
+    c=a*b;
+    cout<<"the of mul of a & b is "<<c<<endl;
+    cout<<"the global c is : "<<::c;*/
+
+  // float ,double , long double literals
+  /* float d= 3.14f;
+   long double e= 3.14l;
+   cout<<"the val of d is:  "<<d<<endl<<"the val of e is: "<<e;
+      cout<< "the size of 3.14 is : "<<sizeof(3.14)<<endl;
+             cout<< "the size of 3.14f is : "<<sizeof(3.14f)<<endl;
+      cout<< "the size of 3.14F is : "<<sizeof(3.14F)<<endl;
+      cout<< "the size of 3.14l is : "<<sizeof(3.14l)<<endl;
+      cout<< "the size of 3.14L is : "<<sizeof(3.14L)<<endl;*/
+
+  // refernce variables
+  /*  int x =5;
+    int &y = x;
+    cout<<x<<endl;
+    cout<<y;*/
+
+  // typecasting
+  /* float z = 3.1459;
+   cout<<z<<endl;
+   cout<<int(z)<<endl;
+   int s= int(z);  // assigning intger value of z to s
+   cout<<s;*/
+
+  /*  float  t=30.44;
+     int r=30;
+     cout<<r+t<<endl;
+     cout<<r+int(t)<<endl;
+     cout<<r+(int)t;*/
+
+  return 0;
+}
