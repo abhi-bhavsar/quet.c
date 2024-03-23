7d68bf52ed99aef7de2caaed8d1a0add01c1dc84
abhi-bhavsar <163317055+abhi-bhavsar@users.noreply.github.com>
2024-03-23 15:27:39 +0530

    Loops in c++

---
+++ 8.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+// loops in c++ 1.for loop ; 2.while loop ; 3.do-while loop
+int main()
+{
+    // program to pirnt first 10 en=ven and odd numbers
+    int i = 1;
+    // structure of for loop
+    /*for(i=1;i<=20;i++){
+        if(i%2 == 0){
+            cout<<i<<endl;
+        }
+    }  cout<<endl;
+     for(i=1;i<=20;i++){
+        if(i%2 != 0){
+            cout<<i<<endl;
+        }
+     }*/
+
+    // while loop
+    /*while(i<=10){
+        cout<<i<<endl;
+        i++;
+    }*/
+    /* while(i>0){
+        cout<<i<<endl;  //infinite loop using while loop
+        i++;
+    }*/
+    /*  while(true){
+          cout<<i<<endl;
+          i++;
+      }*/
+    // to rint table of 6
+    //   int j,k,n=6;
+    /* for(i=1;i<=10;i++){
+     k=n*i;
+     cout<<k<<endl;
+     }*/
+    // to print table from 2 t0 10
+    /* for(i=2;i<=10;i++){
+         for(j=1;j<=10;j++){
+             cout<<i*j<<endl;
+         } cout<<endl;
+    */
+    return 0;
+}
